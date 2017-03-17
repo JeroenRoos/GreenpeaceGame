5bee1fc915b677c2afb504e3eddf4f087dcde03f
Matthijs <matthijs_bierman@live.nl>
2017-03-17 12:20:05 +0100

    Program -> GameController / GameTimer

--- Assets/Scripts/Controllers/GameController.cs
+++ Assets/Scripts/Controllers/GameController.cs
@@ -1,20 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Timers;
 
 public class GameController : MonoBehaviour {
 
     public Game game;
+    private GameTimer timer;
 
     // Use this for initialization
     void Start()
     {
         game = new Game();
-        
+        timer = new GameTimer();
+        timer.StartTimeflowTimer();
+        timer.Elapsed += new ElapsedEventHandler(UpdateGameTime);
+
+    }
+
+    void UpdateGameTime(object source, ElapsedEventArgs e)
+    {
+        if (game.currentYear > 30)
+        {
+            timer.Stop();
+        }
+        else
+        {
+            bool newEvent = game.UpdateTime();
+
+            if (newEvent)
+            {
+                timer.Stop();
+                game.StartNewEvent();
+                timer.Start();
+            }
+        }
+
     }
 
     // Update is called once per frame
     void Update () {
-		
-	}
+        
+    }
 }
