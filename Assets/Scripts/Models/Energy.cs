7e37faaec77b95a850f1c54b03d4e11c345c92e9
Matthijs <matthijs_bierman@live.nl>
2017-03-23 15:45:14 +0100

    GameStats uitgebreid met RegionAvgs en Update, Energy en Status toegevoegd

---
+++ Assets/Scripts/Models/Energy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Energy {
+
+    public Status status;
+
+    public float fossilSource;
+    public float cleanSource;
+    public float nuclearSource;
+
+    public Energy()
+    {
+        fossilSource  = 94.0f;
+        cleanSource   = 5.0f;
+        nuclearSource = 1.0f;
+
+        status = Status.Bad;
+    }
+}
