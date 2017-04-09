84ae5514282198c9157587e1756a795c90ed4961
Jeroen Roos <jeroen.roos8@hotmail.com>
2017-04-09 20:42:19 +0200

    Fuckload of event icons

--- Assets/Scripts/Controllers/EventObjectController.cs
+++ Assets/Scripts/Controllers/EventObjectController.cs
@@ -272,7 +272,103 @@ public class EventObjectController : MonoBehaviour
             case "WaterPollutionConcern":
                 return allTextures[4];
 
-            default: return allTextures[5];
+            case "AccidentTransport":
+                return allTextures[5];
+
+            case "AgricultureEquipmentProject":
+                return allTextures[6];
+
+            case "AgricultureFasterWork":
+                return allTextures[7];
+
+            case "AgricultureHighPollution":
+                return allTextures[8];
+
+            case "AirPollutionNewTech":
+                return allTextures[9];
+
+            case "AntwerpenExpansion":
+                return allTextures[10];
+
+            case "BadHarvest":
+                return allTextures[11];
+
+            case "BelgiumPollution":
+                return allTextures[12];
+
+            case "CleanAirProject":
+                return allTextures[13];
+
+            case "CleanNatureProject":
+                return allTextures[14];
+
+            case "CleanWaterProject":
+                return allTextures[15];
+
+            case "ClimateDeal":
+                return allTextures[16];
+
+            case "CoalPlant":
+                return allTextures[17];
+
+            case "CompaniesEconomyUnhappy":
+                return allTextures[18];
+
+            case "CompaniesHighPollution":
+                return allTextures[19];
+
+            case "Firework":
+                return allTextures[20];
+
+            case "GasMining":
+                return allTextures[21];
+
+            case "GermanyPollution":
+                return allTextures[22];
+
+            case "HouseholdsHighPollution":
+                return allTextures[23];
+
+            case "HouseholdsPollutionDoubts":
+                return allTextures[24];
+
+            case "MoreGasExtraction":
+                return allTextures[25];
+
+            case "NaturePollutionNewTech":
+                return allTextures[26];
+
+            case "NoMoreGas":
+                return allTextures[27];
+
+            case "NuclearPlant":
+                return allTextures[28];
+
+            case "NuclearReactorAccident":
+                return allTextures[29];
+
+            case "ProductionRuhrArea":
+                return allTextures[30];
+
+            case "Recycling":
+                return allTextures[31];
+
+            case "RotterdamExpansion":
+                return allTextures[32];
+
+            case "SchipholExpansion":
+                return allTextures[33];
+
+            case "SolarPanelProject":
+                return allTextures[34];
+
+            case "TrafficJam":
+                return allTextures[35];
+
+            case "WaterPollutionNewTech":
+                return allTextures[36];
+
+            default: return allTextures[37];
         }
     }
 
