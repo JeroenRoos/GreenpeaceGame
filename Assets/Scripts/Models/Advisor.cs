fd8438f13a88c2b9318d23d08ff5f2fdaed5d3c3
Remy Kroese <rkroese96@gmail.com>
2017-04-10 19:54:00 +0200

    EconomyAdvisor && PollutionAdvisor implemented

---
+++ Assets/Scripts/Models/Advisor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+[Serializable]
+public abstract class Advisor
+{
+    protected enum statisticStatus { good, bad };
+
+    abstract public string[] name { get; protected set; }
+    abstract public string[] displayMessage { get; protected set; } //dutch/english display message
+    abstract public string[] dutchStatusMessages { get; protected set; }
+    abstract public string[] englishStatusMessages { get; protected set; }
+
+    public abstract void DetermineDisplayMessage(int currentYear, int currentMonth, double calculateValue);
+}
