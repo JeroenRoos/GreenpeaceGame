f6739bbf8ffc6f3d48dca78f25f34092e25b6aeb
Remy Kroese <rkroese96@gmail.com>
2017-04-12 11:50:31 +0200

    Jeroen CODE FIX

--- Assets/UI/UpdateUI.cs
+++ Assets/UI/UpdateUI.cs
@@ -1429,6 +1429,8 @@ public class UpdateUI : MonoBehaviour
     private void updateActiveEvents()
     {
         string activeEventsRegio = "";
+
+        /*JEROEN CODE
         bool breaking = false;
         foreach (GameEvent e in game.events)
         {
@@ -1450,7 +1452,15 @@ public class UpdateUI : MonoBehaviour
                         break;
                 }
             }
+        }*/
+
+        //NORMALE CODE
+        foreach (GameEvent ge in regio.inProgressGameEvents)
+        {
+            if (ge.isActive || ge.isIdle)
+                activeEventsRegio += ge.publicEventName[taal] + "\n";
         }
+        //
 
         txtActiveEvents.text = activeEventsRegio;
     }
